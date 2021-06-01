6d14f38143a8e05525842d8b87df2a09c9a79b9e
wafaankoush99 <wafadirawe@gmail.com>
2021-06-01 16:45:36 +0300

    first commit

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const express = require('express');
 const cors = require('cors');
 const http = require('http');
 const socketServer = require('socket.io');
+const uuid = require('uuid').v4;
 
 // Internal
 const utils = require('./utils');
@@ -29,31 +30,69 @@ app.use(express.static(path.join(__dirname, './public')));
 
 const races = []; // [{raceId: "2e3s-1s5f4-d2dd-3pc3", started: true,  finished: false , users: [{name: "wesam", wpm: 35, progress: 0.95, errors: 4, complete: false, timestamp: 54545487841}, ...]}, ...]
 
+
+let NUMBER_OF_USERS_PER_RACE = 2;
+
 /* All socket logic */
 io.on('connection', (socket) => {
   console.log('a user Connected :', socket.id);
 
   // on join race
   socket.on('join-race', (payload) => {
-    // client send object {name: "wesam"}
-    // get a race id from the socket if the user were already in arace then remove the user from the race (race array)
-    // check races if emty create a random race id and add it to races then make the user join this race room
-    // if races was not empty then check the last race if has less than 5 users and not started yet then make the user join the race
-    // else create a new race as if races was empty
-    // emit joined event to the user
+
+    if (races.length === 0 || races[races.length - 1].started === true) {
+      let raceId = uuid();
+      races.push({
+        raceId: raceId,
+        started: false,
+        finished: false,
+        users: [{
+          name: payload.name,
+          id: socket.id,
+        }],
+      });
+      socket.join(raceId);
+      socket.emit('joined', races[races.length - 1]);
+    } else {
+      if (socket.rooms.size > 1) {
+        for (let roomID of socket.rooms) {
+          if (roomID !== socket.id) {
+            socket.leave(roomID);
+          }
+        }
+      }
+      if (races[races.length - 1].started === false && races[races.length - 1].users.length < NUMBER_OF_USERS_PER_RACE) {
+        socket.join(races[races.length - 1].raceId);
+        races[races.length - 1].started = true;
+        races[races.length - 1].users = [...races[races.length - 1].users, { name: payload.name, id: socket.id }];
+        socket.emit('joined', races[races.length - 1]);
+        io.to(races[races.length - 1].raceId).emit('started', races[races.length - 1]);
+      }
+    }
+
+
+
   });
 
   // on receive data
   socket.on('refresh-data', (payload) => {
+
+    // io.sockets.clients(room).forEach(() => {
+    //   socket.emit(payload);
+    // });
+
+
+
     //client send all information about his progress {name: "wesam", wpm: 35, progress: 0.95, errors: 4, complete: false}
     // get the race id from the socket then update the user data in races array
   });
 
   // on disconnect
-  socket.on('disconnect', () => {
+  socket.on('disconnect', (payload) => {
+
+
     // The user disconnected for some reason
     // Get a race id from the socket if the user in a race then remove the user from that race (from the race array)
-    console.log('user disconnected: ', socket.id);
   });
 });
 
